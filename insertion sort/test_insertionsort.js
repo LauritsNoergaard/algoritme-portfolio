05039bc8ef24d06a76e4e1334d9fd96c89420de4
LauritsNoergaard <laur8709@stud.kea.dk>
2024-11-10 14:31:17 +0100

    also test.js

--- insertion sort/test_insertionsort.js
+++ insertion sort/test_insertionsort.js
@@ -1,4 +1,4 @@
 import * as insertionSort from "./insertionsort.js"
 
-let list = [42];
-insertionSort.insertionSortShift(list);
+let list = [10, -1, 2, -10, 0, 3, 4, -5];
+insertionSort.insertionSortswap(list);
